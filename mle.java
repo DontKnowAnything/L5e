15d7ff99ec7c6ce90ee018ef748aed2b14e9b40a
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-03 17:29:00 -0500

    Update mle.java

--- mle.java
+++ mle.java
@@ -18,159 +18,132 @@ public class MailingLabel implements Serializable
 	/**
 	 * 
 	 */
-	private String name;
-	private String address;
-	private String city;
-	private String state;
-	private String zip;
-	private String nacsz;
+   private String name;
+   private String address;
+   private String city;
+   private String state;
+   private String zip;
 
 	/**
-	 * 
+	 * The
 	 */
-	public MailingLabel() {
-		name="";
-		address="";
-		city="";
-		state="";
-		zip="";
-	}
+   public MailingLabel() {
+      name = "";
+      address = "";
+      city = "";
+      state = "";
+      zip ="";
+   }
 
 	/**
 	 * The overloaded constructor separates data members with commas.
 	 * @param dataMembers Holds data members
 	 * @throws IllegalMailingLabelException If the parameter does not have 5 fields
 	 */
-	public MailingLabel( String dataMembers ) throws IllegalMailingLabelException 
-	{
-		if ( dataMembers.length()!=5 )
-		{
-			dataMembers+=name+","+address+","+city+","+state+","+zip;
-			//dataMembers.split(",");
-		}
-		else
-		{
-			throw new IllegalMailingLabelException( "this" ) ;
-		}
-	}
+   public MailingLabel( String dataMembers ) throws IllegalMailingLabelException 
+   {
+      
+      dataMembers+=name + "," + address + "," + city + "," + state + "," + zip;
+      String [] dataMembersArray = dataMembers.split(",");
+      if ( dataMembersArray.length>=5 )
+      {
+         for(int i=0; i<dataMembersArray.length; i++){
+            dataMembersArray[0]=name;}
+      }
+      else
+      {
+         throw new IllegalMailingLabelException( "this" ) ;
+      }
+   }
 
 	/**
 	 * @return name of the person
 	 */
-	public String getName()
-	{
-		String gn=name;
-		if(gn==null)
-		{
-			gn+=" this is the getName() method and name is empty ";
-		}
-		else if(gn.isEmpty())
-		{
-			gn+="this is the getName () method and name is null. ";
-		}
-		else
-		{
-			gn+=" this is the getName() method and name is not empty or null";
-		}
-		return gn;
-	}
+   public String getName()
+   {
+      String gn = name;
+      return gn;
+   }
 
 	/**
 	 * @param name the name to set
 	 */
-	public void setName(String n) {
-		String sn=n;
-		if(sn.isEmpty()){
-			sn+="name is empty";
-		}
-		else{
-			sn+=" This is the setName method and name is not empty.\n";
-		}
-		name = sn;
-	}
+   public void setName( String n ) {
+      String sn = n;
+      name = sn;
+   }
 
 	/**
 	 * @return the address
 	 */
-	public String getAddress() {
-		String ga=address;
-		if(ga.isEmpty()){
-			ga+="name is empty";
-		}
-		else{
-			ga+="name is not empty.";
-		}
-		return ga;
-	}
+   public String getAddress() {
+      String ga = address;
+      return ga;
+   }
 
 	/**
 	 * @param address the address to set
 	 */
-	public void setAddress(String setAddress) {
-		String addressCopy = setAddress;
-		if(addressCopy.isEmpty()){
-			addressCopy+="name is empty";
-		}
-		else{
-			addressCopy+="name is not empty.";
-		}
-		address=addressCopy;
-	}
+   public void setAddress( String setAddress ) {
+      String addressCopy = setAddress;
+      address = addressCopy;
+   }
 
 	/**
 	 * @return the city
 	 */
-	public String getCity() {
-		String gc=city;
-		return gc;
-	}
+   public String getCity() {
+      String gc = city;
+      return gc;
+   }
 
 	/**
 	 * @param sc the city to set
 	 */
-	public void setCity(String sc) {
-		String scm=sc;
-		city=scm;
-	}
+   public void setCity(String sc) {
+      String scm = sc;
+      city = scm;
+   }
 
 	/**
 	 * @return the state
 	 */
-	public String getState() {
-		String gs=state;
-		return gs;
-	}
+   public String getState() {
+      String gs = state;
+      return gs;
+   }
 
 	/**
 	 * @param state the state to set
 	 */
-	public void setState(String ss) {
-		String sm = ss;
-		state=sm;
-	}
+   public void setState(String ss) {
+      String sm = ss;
+      state = sm;
+   }
 
 	/**
 	 * @return the zip
 	 */
-	public String getZip() 
-	{
-
-		return zip;
-	}
+   public String getZip() 
+   {
+      String gz = zip;
+      return gz;
+   }
 
 	/**
 	 * The zip code
 	 * @param zip the persons zip
 	 */
-	public void setZip( String zip ) {
-		this.zip = zip;
-	}
-	@Override
-	public String toString() {
-		//setName(getName());
-		//return getName() + name+", address=" + address
-		//		+ ", city=" + city + ", state=" + state + ", zip=" + zip;
-		return name+address;
-	}
+   public void setZip( String zipCopy ) {
+      String sz = zipCopy;
+      zip = sz;
+   }
+   @Override
+   public String toString() {
+      //setName(getName());
+      String ts = name + address
+         	+ city + " ," + state + "," + zip;
+      return ts;
+   }
 
 }
