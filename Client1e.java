5e8baacf849c7daa8b40c71c81d1db5edd64264b
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-03 17:28:12 -0500

    Update Client1e.java

--- Client1e.java
+++ Client1e.java
@@ -11,61 +11,78 @@ import java.util.ArrayList;
  */
 public class Client1 {
 
-	public static void main(String[] args) throws IllegalMailingLabelException
-	{
-
-		FileReader readFile=null;
-		BufferedReader bufferReader=null;
-		ArrayList<MailingLabel>listOfLabels;
-		MailingLabel mailLabel;
-		String nameRead="";
-		String addressRead="";
-		String cityRead="";
-		String lineRead="";
-		String all="";
-		System.out.println("Program Starting");
-		try 
-		{
-			readFile=new FileReader("C:\\Stuff\\L5e\\Lab5e\\src\\textForMailingLabel.txt");
-			bufferReader=new BufferedReader(readFile);
-			listOfLabels=new ArrayList<MailingLabel>();
-
-			nameRead=bufferReader.readLine();
-			addressRead=bufferReader.readLine();
-			cityRead=bufferReader.readLine();
-			lineRead=bufferReader.readLine();
-			lineRead=bufferReader.readLine();
-			all=nameRead+addressRead+cityRead;
-			/*if(!all.isEmpty())
-			{*/
-				mailLabel=new MailingLabel(all);
-				
-					listOfLabels.add(mailLabel);
-					if(listOfLabels.isEmpty())
-					{
-						System.out.println("list is empty");
-					}
-					else
-					{
-
-						System.out.println(listOfLabels.toString()+"\n");
-						System.out.println(mailLabel);
-					}
-				
-				
-			/*}
-			else
-			{
-				System.out.println("File is empty");
-			}*/
-		} 
-		catch (IOException io)
-		{
-			io.printStackTrace();
-		}
-		finally
-		{
-			System.out.println("Program Ending");
-		}
-	}
+   public static void main(String[] args) throws IllegalMailingLabelException
+   {
+      FileReader readFile = null;
+      BufferedReader bufferReader = null;
+      ArrayList<MailingLabel>listOfLabels;
+      MailingLabel mailLabel;
+      MailingLabel mailLabel2;
+      String nameRead = "";
+      String addressRead = "";
+      String cityRead = "";
+      String stateRead = "";
+      String zipRead = "";
+      String all = "";
+      System.out.println("Program Starting");
+      
+      try 
+      {
+         readFile = new FileReader("H:\\Lab5j\\src\\textForMailingLabel.txt");
+         bufferReader = new BufferedReader(readFile);
+         listOfLabels = new ArrayList<MailingLabel>();
+         mailLabel = new MailingLabel();
+      
+         nameRead = bufferReader.readLine();
+         mailLabel.setName( nameRead );
+         String getNameRead = mailLabel.getName();
+         
+         addressRead = bufferReader.readLine();
+         mailLabel.setAddress( addressRead );
+         String getAddressRead = mailLabel.getAddress();
+         
+         cityRead = bufferReader.readLine();
+         mailLabel.setCity( cityRead );
+         String getCityRead = mailLabel.getCity();
+         
+         stateRead = bufferReader.readLine();
+         mailLabel.setState( stateRead );
+         String getStateRead = mailLabel.getState();
+         
+         zipRead = bufferReader.readLine();
+         mailLabel.setState( zipRead );
+         String getZipRead = mailLabel.getZip();
+         
+         all += getNameRead + getAddressRead + getCityRead + stateRead + zipRead;
+      	/*if(!all.isEmpty())
+      	{*/
+         mailLabel2 = new MailingLabel( all );
+      		
+         listOfLabels.add( mailLabel2 );
+         if(listOfLabels.isEmpty())
+         {
+            System.out.println( "list is empty" );
+         }
+         else
+         {
+            for (MailingLabel mailingLabelObj:listOfLabels )
+               System.out.println( mailLabel );
+         }
+      		
+      		
+      	/*}
+      	else
+      	{
+      		System.out.println("File is empty");
+      	}*/
+      } 
+      catch ( IOException io )
+      {
+         io.printStackTrace();
+      }
+      finally
+      {
+         System.out.println( "Program Ending" );
+      }
+   }
 }
